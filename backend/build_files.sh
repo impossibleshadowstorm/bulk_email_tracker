c865ad814ceb13c1f90ada5ebbceaa85304b7777
Sumit Saurav <impossibleshadowstorm@gmail.com>
2024-10-28 23:08:20 +0530

    updated script

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,7 +1,13 @@
 echo "BUILD STARTED..."
 
+# Ensure pip is installed for Python 3.9
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip setuptools wheel
+
+# Install requirements
 python3.9 -m pip install -r requirements.txt
 
-python3.9 manage.py collectstatic--noinput --clear
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD ENDED..."
